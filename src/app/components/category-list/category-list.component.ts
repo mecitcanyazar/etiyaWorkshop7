580d4cf6a61407c7bbe0bf0687e65610872cd7fe
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-11-24 17:56:20 +0300

    fix selected category list item is active when double click

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss'],
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
   title: string = 'Category List';
   categories: any[] = [
+    // property
     { id: 1, name: 'Beverages' },
     { id: 2, name: 'Condiments' },
     { id: 3, name: 'Confections' },
@@ -17,12 +20,14 @@ export class CategoryListComponent {
     { id: 7, name: 'Produce' },
     { id: 8, name: 'Seafood' },
     { id: 9, name: 'Snacks' },
-    { id: 10, name: 'Sweets' },
+    { id: 10, name: 'Sweets', productCount: 100 },
   ];
 
+  //# Encapsulation
   private _categoriesListItems: any[] = [{ label: 'All', value: null }];
   //# Getter
   get categoriesListItems(): any[] {
+    // property
     return [
       ...this._categoriesListItems,
       ...this.categories.map((c) => {
@@ -37,25 +42,51 @@ export class CategoryListComponent {
   // console.log(this.categoriesListItems); // Get
   // this.categoriesListItems = []; // Set
 
-  selectedCategoryId: number | null = null;
+  //: private, public, protected
+  //: private: sadece class içerisinde kullanılabilir.
+  //: public: her yerden kullanılabilir.
+  //: Default olarak herşey public'tir.
+  //: protected: sadece class içerisinde ve class'ın inherit edildiği yerlerde kullanılabilir.
+  public selectedCategoryId: number | null = null;
 
-  onSelectedCategory(categoryId: number | null): void {
-    // if (category === null) this.selectedCategoryId = null;
-    // else this.selectedCategoryId = category.id;
-
-    //# Debugging
-    //debugger; // breakpoint. Uygulama çalışma anında bu satıra geldiğinde uygulama durucak ve adım adım takip edebileceğimiz bir panel açılacak.
-
-    //# ternary operator
-    // this.selectedCategoryId = category === null ? null : category.id;
-
-    //# optional chaining operator
-    //: object?.id dediğimiz zaman, object null değilse ve id'e ulaşabiliyorsa id'sini alır, null ise null döner.
-
-    //# nullish coalescing operator
-    //: ?? operatörü ile sol taraf false (null, undefined, 0, "") ise sağ tarafı atar.
-    this.selectedCategoryId = categoryId ?? null;
+  // private activatedRoute: ActivatedRoute;
+  //: IoC (Inversion of Control), referansların tutulduğu bir container'dır.
+  //: Dependency Injection, IoC container'ın içerisindeki referansları kullanmamızı sağlayan bir mekanizmadır.
+  constructor(private activatedRoute: ActivatedRoute) {
+    //: constructor class oluşturulduğu an çalışır.
+    // this.activatedRoute = activatedRoute;
   }
+
+  ngOnInit(): void {
+    //: ngOnInit() methodu component'in yerleştirildiği an çalışır.
+    this.getSelectedCategoryIdFromRoute();
+  }
+
+  getSelectedCategoryIdFromRoute() {
+    //* Observer Design Pattern
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId'] !== undefined)
+        this.selectedCategoryId = Number(params['categoryId']);
+    }); //* Callback
+  }
+
+  // onSelectedCategory(categoryId: number | null): void {
+  //   // if (category === null) this.selectedCategoryId = null;
+  //   // else this.selectedCategoryId = category.id;
+
+  //   //# Debugging
+  //   //debugger; // breakpoint. Uygulama çalışma anında bu satıra geldiğinde uygulama durucak ve adım adım takip edebileceğimiz bir panel açılacak.
+
+  //   //# ternary operator
+  //   // this.selectedCategoryId = category === null ? null : category.id;
+
+  //   //# optional chaining operator
+  //   //: object?.id dediğimiz zaman, object null değilse ve id'e ulaşabiliyorsa id'sini alır, null ise null döner.
+
+  //   //# nullish coalescing operator
+  //   //: ?? operatörü ile sol taraf false (null, undefined, 0, "") ise sağ tarafı atar.
+  //   this.selectedCategoryId = categoryId ?? null;
+  // }
 
   isSelectedCategory(categoryId: number | null): boolean {
     return categoryId === this.selectedCategoryId;
